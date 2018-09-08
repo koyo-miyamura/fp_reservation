95a06d0652c1b1bfcf253a068c4ec66cd271b40b
koyo <nexus9071@gmail.com>
2018-09-09 05:01:39 +0900

    User/Fp新規登録ロジック（統合テスト未実装）

--- app/controllers/fps_controller.rb
+++ app/controllers/fps_controller.rb
@@ -1,4 +1,27 @@
 class FpsController < ApplicationController
+
   def new
+    @fp = Fp.new
   end
+
+  def show
+    @fp = Fp.find(params[:id])
+  end
+
+  def create
+    @fp = Fp.new(fp_params)
+    if @fp.save
+      flash[:success] = "FPの新規登録に成功しました"
+      redirect_to @fp
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def fp_params
+      params.require(:fp).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
--- test/controllers/fps_controller_test.rb
+++ test/controllers/fps_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class FpsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @fp = fps(:fp1)
+  end
+
   test "should get new" do
     get fps_signup_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get fp_path(@fp)
     assert_response :success
   end
 
