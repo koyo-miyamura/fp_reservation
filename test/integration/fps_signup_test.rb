88d4ad6cab271db698aa1442e5c77bea1e8a1e0b
koyo <nexus9071@gmail.com>
2018-09-09 16:06:48 +0900

    User/Fp signup 統合テスト

--- test/integration/fps_signup_test.rb
+++ test/integration/fps_signup_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class FpsSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup" do
+    get fps_signup_path
+    assert_no_difference 'Fp.count' do
+      post fps_signup_path, params: { fp: { name:  "",
+                                         email: "fp@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'fps/new' # errorが発生したらnewへ戻る
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    assert_select 'form[action="/fps/signup"]'
+  end
+
+  test "valid signup" do
+    get fps_signup_path
+    assert_difference 'Fp.count', 1 do
+      post fps_path, params: { fp: { name:  "Example fp",
+                                         email: "fp@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect! # リダイレクト先に移る
+    assert_template 'fps/show'
+    assert_not flash.empty?
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup" do
+    get users_signup_path
+    assert_no_difference 'User.count' do
+      post users_signup_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new' # errorが発生したらnewへ戻る
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    assert_select 'form[action="/users/signup"]'
+  end
+
+  test "valid signup" do
+    get users_signup_path
+    assert_difference 'User.count', 1 do
+      post users_signup_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect! # リダイレクト先に移る
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
