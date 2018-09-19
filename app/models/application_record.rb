b5e1a1d993a0e44e20c58e72310de18eed7e09f7
koyo <nexus9071@gmail.com>
2018-09-19 16:55:33 +0900

    fixtureのpassword_digest用メソッドをApplicationRecordから削除

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # test用
-  # has_secure_password の内部実装参考
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 end
